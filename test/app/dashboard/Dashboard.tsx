755c17c84b4ced8e2242907a46cba43bd79b8e34
Sehrish Jetnetix <sehrishnadeem39@gmail.com>
2025-08-08 13:32:16 +0500

    finally10000011111111111

--- test/app/dashboard/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Dashboard() {
-  return (
-    <div>
-      <h1 className="text-xl font-bold mb-4">Dashboard</h1>
-      
-    </div>
-  );
-}
--- test/app/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import Dashboard from './dashboard/Dashboard';
-
-export default function HomePage() {
-  return <Dashboard />;
-}
